eccb3e39b60c9a361dade558d56e0f4a14475012
Douglas Liu <douglaszsliu@gmail.com>
2018-04-05 23:27:53 +0800

    DOUGLAS: Add more derived Cells

---
+++ src/JungleGame/Game/DenCell.java
@@ -0,0 +1,10 @@
+package JungleGame.Game;
+
+/**
+ * Created by Douglas Liu on 5/4/2018.
+ */
+public class DenCell extends Cell {
+    public DenCell(Animal animal) {
+        super(animal);
+    }
+}
---
+++ src/JungleGame/Game/TrapCell.java
@@ -0,0 +1,10 @@
+package JungleGame.Game;
+
+/**
+ * Created by Douglas Liu on 5/4/2018.
+ */
+public class TrapCell extends Cell{
+    public TrapCell(Animal animal) {
+        super(animal);
+    }
+}
